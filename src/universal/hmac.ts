d6133995e33ce1fb232c9c91da5297b1762a2e7f
Reaper Gelera <ahoy@barelyhuman.dev>
2024-01-14 01:41:02 +0530

    fix: tests for the universal export

--- src/universal/hmac.ts
+++ src/universal/hmac.ts
@@ -1,4 +1,4 @@
-import subtle from 'uncrypto'
+import { subtle } from 'uncrypto'
 
 const algoMap = {
   sha1: 'SHA-1',
@@ -13,14 +13,6 @@ export async function createHmac(
   secret: string,
   data: Buffer
 ) {
-  // let enc
-  // if (TextEncoder.constructor.length == 1) {
-  //   // @ts-ignore
-  //   enc = new TextEncoder('utf-8')
-  // } else {
-  //   enc = new TextEncoder()
-  // }
-
   const key = await subtle.importKey(
     'raw', // raw format of the key - should be Uint8Array
     secret,
--- tests/universal.test.js
+++ tests/universal.test.js
@@ -1,6 +1,10 @@
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
-import { generateTOTPSecret, isTOTPValid, totp } from '../src/index.js'
+import {
+  generateTOTPSecret,
+  isTOTPValid,
+  totp,
+} from '../src/universal/universal.ts'
 
 test('generate 2 different secrets', async () => {
   const secret = generateTOTPSecret()
@@ -14,14 +18,14 @@ test('dynamic isValid', async () => {
   const opts = {
     period: period,
   }
-  const otp = totp(secret, undefined, opts)
-  assert.ok(isTOTPValid(secret, otp, opts))
+  const otp = await totp(secret, undefined, opts)
+  assert.ok(await isTOTPValid(secret, otp, opts))
 })
 
 test('static is valid', async () => {
   const secret = 'JFLVYRQGJ5ZFOLSYO5HVOWIZGAYHOCTEGNLE2JYMNAMTCET3A5VQ===='
   const d = 1704875845134
-  const otp = totp(secret, d / 1000)
+  const otp = await totp(secret, d / 1000)
   assert.equal(otp, '' + 881718)
 })
 
